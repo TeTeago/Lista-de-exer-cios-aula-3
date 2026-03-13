139b48d7b3dbf8b3a39b1fe7354d9dd0cd378eac
TeTeago <55003392+TeTeago@users.noreply.github.com>
2026-03-13 13:18:15 -0300

    Nome e Matrícula

--- exercicio4.java
+++ exercicio4.java
@@ -1,22 +1,27 @@
+
+
 import java.util.Scanner;
 
 public class exercicio4 {
 
     public static void main(String[] args) {
 
+        
         Scanner scanner = new Scanner(System.in); {
 
-         System.out.println("Digite o salário atual: ");
-         double salarioAtual = scanner.nextDouble();
-
-        System.out.println("Salário original: ");
-        System.out.println(salarioAtual);
         
-        System.out.println("Novo salário: ");
-        double novoSalario = salarioAtual * 1.07;
-        System.out.println(novoSalario);
+            System.out.println("Digite o salário atual: ");
+            double salarioAtual = scanner.nextDouble();
 
-        scanner.close();
+            System.out.println("Salário original: ");
+            System.out.println(salarioAtual);
+        
+            System.out.println("Novo salário: ");
+            double novoSalario = salarioAtual * 1.07;
+            
+            System.out.println(novoSalario);
+            
+            scanner.close();
 
 
 }
