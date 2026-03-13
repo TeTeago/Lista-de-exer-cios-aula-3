df3f70e4cdbaec2a08a6e72483a5088b3e90d25b
TeTeago <55003392+TeTeago@users.noreply.github.com>
2026-03-13 13:24:56 -0300

    Add files via upload

---
+++ exercicio5.java
@@ -0,0 +1,35 @@
+// Nome: Tiago Falco França
+// Matrícula: 1261942635
+
+import java.util.Scanner;
+
+public class exercicio5 {
+
+
+    public static void main(String[] args) {
+
+    
+        Scanner scanner = new Scanner(System.in); {
+            
+            System.out.println("Digite a nota 1: ");
+            double nota1 = scanner.nextDouble();
+
+            System.out.println("Digite a nota 2: ");
+            double nota2 = scanner.nextDouble();
+
+            System.out.println("Digite a nota 3: ");
+            double nota3 = scanner.nextDouble();    
+
+            double media = (nota1 + nota2 + nota3) / 3;
+            System.out.printf("A média é: %.2f", media);
+
+        
+            scanner.close();
+
+
+}
+
+
+}
+
+}
